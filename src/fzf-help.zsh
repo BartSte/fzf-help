744b448aa38ffd786b3c8f978d8b33bd595cb4db
barts <bartsteensma@outlook.com>
2023-07-02 16:05:10 +0200

    bugfix: the whole BUFFER should be passed to fzf-select-option instead of LBUFFER

--- src/fzf-help.zsh
+++ src/fzf-help.zsh
@@ -1,9 +1,9 @@
 this_dir=$(dirname $(realpath ${BASH_SOURCE:-$0}))
 
 fzf-help-widget() {
-    [[ -z $LBUFFER ]] && { zle reset-prompt; return }
-    local option=$(echo $LBUFFER | $this_dir/fzf-select-option)
-    LBUFFER="$LBUFFER$option"
+    [[ -z $BUFFER ]] && { zle reset-prompt; return }
+    local option=$(echo $BUFFER | $this_dir/fzf-select-option)
+    BUFFER="$BUFFER$option"
     local ret=$?
     zle reset-prompt
     return $ret
