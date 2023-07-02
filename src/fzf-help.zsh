794411c9211c477599a81b02c0fd7557225a64e3
barts <bartsteensma@outlook.com>
2023-07-02 17:09:10 +0200

    bugfix: cursor must be at the eol after inserting option

--- src/fzf-help.zsh
+++ src/fzf-help.zsh
@@ -3,7 +3,7 @@ this_dir=$(dirname $(realpath ${BASH_SOURCE:-$0}))
 fzf-help-widget() {
     [[ -z $BUFFER ]] && { zle reset-prompt; return }
     local option=$(echo $BUFFER | $this_dir/fzf-select-option)
-    BUFFER="$BUFFER$option"
+    LBUFFER="$BUFFER$option"
     local ret=$?
     zle reset-prompt
     return $ret
