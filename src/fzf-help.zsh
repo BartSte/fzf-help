cab9b70bb49bd3d4ebd8e769299501fbd7198013
barts <bartsteensma@outlook.com>
2023-07-03 21:29:59 +0200

    refactor: renamed opts and add docstring

--- src/fzf-help.zsh
+++ src/fzf-help.zsh
@@ -1,10 +1,20 @@
 this_dir=$(dirname $(realpath ${BASH_SOURCE:-$0}))
 
+##############################################################################
+# fzf-help-widget
+#
+# fzf-help-widget is a zsh widget that should be bind to a key. It will open
+# fzf with the help options and append the selected options to the command 
+# line.
+##############################################################################
 fzf-help-widget() {
     [[ -z $BUFFER ]] && { zle reset-prompt; return }
-    local option=$(echo $BUFFER | $this_dir/fzf-select-option | tr "\n" " ")
-    LBUFFER="$BUFFER$option"
+
+    local opts=$(echo $BUFFER | $this_dir/fzf-select-option | tr "\n" " ")
+    BUFFER="$BUFFER$opts"
+
     local ret=$?
     zle reset-prompt
+    zle end-of-line
     return $ret
 }
